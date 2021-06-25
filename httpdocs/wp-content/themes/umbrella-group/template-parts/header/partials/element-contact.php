b83c8735d8d2a4469ce8b8a9640d9675508db585
konstantin.annikov <annikovk@gmail.com>
2021-06-25 16:44:14 +0700

    optimization

---
+++ httpdocs/wp-content/themes/umbrella-group/template-parts/header/partials/element-contact.php
@@ -0,0 +1,4 @@
+<?php ?>
+        <div class=" header-mail-top-right"><a href="mailto:contact@taxlab.ru"> contact@taxlab.ru </a></div>
+        <div class=" header-phone-top-right"><a href="tel:+73833731717" class=""> +7 (383) 373-17-17 </a></div>
+<?php
