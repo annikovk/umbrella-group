e6f315a8fba1dd2c95097281908ee46bd66e4197
konstantin.annikov <konstantin.annikov@jetbrains.com>
2021-09-12 14:53:57 +0700

    [TZ 3-7] "specialists" shortcode

---
+++ httpdocs/wp-content/themes/umbrella-group/includes/custom_post_types/specialist.php
@@ -0,0 +1,197 @@
+<?php
+
+function specialist_custom_post_type()
+{
+// Set UI labels for Custom Post Type
+    $labels = array(
+        'name' => _x('Специалисты', 'Post Type General Name'),
+        'singular_name' => _x('Специалист', 'Post Type Singular Name'),
+        'menu_name' => __('Специалисты'),
+        'all_items' => __('Все специалисты'),
+        'add_new_item' => __('Добавить нового специалиста'),
+        'add_new' => __('Добавить нового'),
+        'edit_item' => __('Редактировать'),
+        'update_item' => __('Обновить'),
+        'search_items' => __('Поиск специалистов'),
+        'not_found' => __('Специалистов не найдено'),
+    );
+
+// Set other options for Custom Post Type
+    $args = array(
+        'label' => __('Специалисты'),
+        'description' => __(''),
+        'labels' => $labels,
+        'rewrite' => array('slug' => 'specialist'),
+        'supports' => array('title', 'thumbnail'),
+        'hierarchical' => false,
+        'public' => true,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'show_in_nav_menus' => true,
+        'show_in_admin_bar' => true,
+        'menu_position' => 5,
+        'menu_icon' => 'dashicons-businessperson',
+        'can_export' => true,
+        'has_archive' => false,
+        'exclude_from_search' => true,
+        'publicly_queryable' => false,
+        'capability_type' => 'post',
+    );
+
+// Registering your Custom Post Type
+    register_post_type('specialist', $args);
+
+}
+
+
+// Add the custom columns to the specialist post type:
+add_filter('manage_specialist_posts_columns', 'set_custom_edit_specialist_columns');
+function set_custom_edit_specialist_columns($columns)
+{
+    unset($columns['date']);
+
+    $columns['specialist_branch'] = 'Направление';
+    $columns['specialist_name'] = 'Имя Отчество';
+    return $columns;
+}
+
+// Add the data to the custom columns for the specialist post type:
+add_action('manage_specialist_posts_custom_column', 'custom_specialist_column', 10, 2);
+function custom_specialist_column($column, $post_id)
+{
+    switch ($column) {
+
+        case 'specialist_branch' :
+            print_r(get_post_meta($post_id, 'specialist_branch', true));
+            break;
+        case 'specialist_name' :
+            print_r(get_post_meta($post_id, 'specialist_name', true));
+            break;
+        case 'specialist_tags':
+            $terms = get_the_term_list($post_id, 'specialist_tags', '', ',', '');
+            if (is_string($terms))
+                echo $terms;
+            else
+                echo '';
+            break;
+
+    }
+}
+
+
+add_action('init', 'specialist_custom_post_type');
+add_action('init', function () {
+    add_ux_builder_post_type('specialist');
+});
+function get_specialist_metabox_value($post)
+{
+    ?>
+    <div class="hcf_box">
+        <style scoped>
+            .hcf_box {
+                display: grid;
+                /*grid-template-columns: max-content 100%;*/
+                grid-row-gap: 10px;
+                grid-column-gap: 20px;
+            }
+
+            .hcf_field {
+                display: contents;
+            }
+        </style>
+        <div class="meta-options hcf_field">
+            <label for="specialist_branch">Направление</label>
+            <select id="specialist_branch" name="specialist_branch">
+                <option <?php if (esc_attr(get_post_meta(get_the_ID(), 'specialist_branch', true))=="-") {echo "selected";}; ?> value="-">-</option>
+                <option <?php if (esc_attr(get_post_meta(get_the_ID(), 'specialist_branch', true))=="Бухгалтерия") {echo "selected";}; ?> value="Бухгалтерия">Бухгалтерия</option>
+                <option <?php if (esc_attr(get_post_meta(get_the_ID(), 'specialist_branch', true))=="Аудит") {echo "selected";}; ?> value="Аудит">Аудит</option>
+                <option <?php if (esc_attr(get_post_meta(get_the_ID(), 'specialist_branch', true))=="Юридические услуги") {echo "selected";}; ?> value="Юридические услуги">Юридические услуги</option>
+                <option <?php if (esc_attr(get_post_meta(get_the_ID(), 'specialist_branch', true))=="Лицензирование") {echo "лицензирование";}; ?> value="Лицензирование">Лицензирование</option>
+                <option <?php if (esc_attr(get_post_meta(get_the_ID(), 'specialist_branch', true))=="Регистрация и ликвидация") {echo "selected";}; ?> value="Регистрация и ликвидация">Регистрация и ликвидация</option>
+            </select>
+        </div>
+        <div class="meta-options hcf_field">
+            <label for="specialist_name">Имя Отчество</label>
+            <input id="specialist_name"
+                   type="text"
+                   name="specialist_name"
+                   value="<?php echo esc_attr(get_post_meta(get_the_ID(), 'specialist_name', true)); ?>">
+        </div>
+        <div class="meta-options hcf_field">
+            <label for="specialist_position">Должность</label>
+            <input id="specialist_position"
+                   type="text"
+                   name="specialist_position"
+                   value="<?php echo esc_attr(get_post_meta(get_the_ID(), 'specialist_position', true)); ?>">
+        </div>
+        <div class="meta-options hcf_field">
+            <label for="specialist_experience">Опыт</label>
+            <input id="specialist_experience"
+                   type="text"
+                   name="specialist_experience"
+                   value="<?php echo esc_attr(get_post_meta(get_the_ID(), 'specialist_experience', true)); ?>">
+        </div>
+        <div class="meta-options hcf_field">
+            <label for="specialist_description">Описание</label>
+            <textarea id="specialist_description"
+                      type="text"
+                      name="specialist_description"><?php echo esc_attr(get_post_meta(get_the_ID(), 'specialist_description', true)); ?></textarea>
+        </div>
+        <div class="meta-options hcf_field">
+            <label for="specialist_education">Образование</label>
+            <input id="specialist_education"
+                   type="text"
+                   name="specialist_education"
+                   value="<?php echo esc_attr(get_post_meta(get_the_ID(), 'specialist_education', true)); ?>">
+        </div>
+        <div class="meta-options hcf_field">
+            <label for="specialist_specialization">Специализация</label>
+            <input id="specialist_specialization"
+                   type="text"
+                   name="specialist_specialization"
+                   value="<?php echo esc_attr(get_post_meta(get_the_ID(), 'specialist_specialization', true)); ?>">
+        </div>
+        <div class="meta-options hcf_field">
+            <label for="specialist_office">Офис</label>
+            <input id="specialist_office"
+                   type="text"
+                   name="specialist_office"
+                   value="<?php echo esc_attr(get_post_meta(get_the_ID(), 'specialist_office', true)); ?>">
+        </div>
+        <div class="meta-options hcf_field">
+            <label for="specialist_main">Ведущий</label>
+            <select id="specialist_main" name="specialist_main">
+                <option <?php if (esc_attr(get_post_meta(get_the_ID(), 'specialist_main', true))=="Нет") {echo "selected";}; ?> value="Нет">Нет</option>
+                <option <?php if (esc_attr(get_post_meta(get_the_ID(), 'specialist_main', true))=="Да") {echo "selected";}; ?> value="Да">Да</option>
+            </select>
+        </div>
+
+    </div>
+    <?php
+}
+
+function specialist_save_meta_box( $post_id ) {
+    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return;
+    if ( $parent_id = wp_is_post_revision( $post_id ) ) {
+        $post_id = $parent_id;
+    }
+    $fields = [
+        'specialist_branch',
+        'specialist_office',
+        'specialist_description',
+        'specialist_education',
+        'specialist_specialization',
+        'specialist_name',
+        'specialist_position',
+        'specialist_experience',
+        'specialist_main',
+    ];
+    foreach ( $fields as $field ) {
+        if ( array_key_exists( $field, $_POST ) ) {
+            update_post_meta( $post_id, $field, sanitize_text_field( $_POST[$field] ) );
+        }
+    }
+}
+add_action( 'save_post', 'specialist_save_meta_box' );
+add_action('add_meta_boxes', function () { add_meta_box('specialist_form_metabox', 'Данные', 'get_specialist_metabox_value', 'specialist', 'side');});
+
---
+++ httpdocs/wp-content/themes/umbrella-group/template-parts/shortcodes/specialist.php
@@ -0,0 +1,156 @@
+<?php
+
+class umbrella_specialist
+{
+    private $css_file = ['/assets/css/blocks/specialist.css'];
+    private $category = "";
+    public $atts;
+    public $err;
+
+    public function fill_attributes()
+    {
+        if (isset($this->atts['category'])) {
+            $this->category = $this->atts['category'];
+            return true;
+        } else {
+            $this->category = $this->get_category_by_url();
+            return true;
+        }
+    }
+
+    public function generate_shortcode()
+    {
+        $tiles = "";
+        $posts = $this->get_specialists_posts($this->category);
+        foreach ($posts as $post) {
+            $tiles .= $this->get_tile($post);
+        }
+        umbrella_add_custom_css_files($this->css_file);
+        return $tiles;
+    }
+
+    private function get_tile($post): string
+    {
+        if (get_post_meta($post->ID, 'specialist_main', true) == "Да") $main = true; else $main = false;
+        if (get_the_post_thumbnail_url($post->ID) == "") {
+            $image_attributes = wp_get_attachment_image_src(11223, 'full');
+            $photo = '<img src="' . $image_attributes[0] . '"  />';
+        } else {
+            $photo = get_the_post_thumbnail($post->ID);
+        }
+        $education = get_post_meta($post->ID, 'specialist_education', true);
+        $description = get_post_meta($post->ID, 'specialist_description', true);
+        $name = get_post_meta($post->ID, 'specialist_name', true);
+        $full_name = $name . " " . get_the_title($post->ID);
+        $experience = get_post_meta($post->ID, 'specialist_experience', true);
+        $specialization = get_post_meta($post->ID, 'specialist_specialization', true);
+        $contact_form = do_shortcode('[contact-form-7 id="11227" title="Форма Задать вопрос специалисту"]');
+        $contact_form = str_replace("{Имя отчество}", $name, $contact_form);
+        $lightbox = <<<EOHTML
+                [lightbox id="specialist-form-$post->ID" width="400px" padding="0px"]
+                    $contact_form
+                [/lightbox]
+        EOHTML;
+        $lightbox = do_shortcode($lightbox);
+        $label = $this->get_label($this->category);
+        if ($main) $label = "Ведущий " . $label;
+        if ($main) {
+            $html = <<<EOHTML
+            <div class="specialist-tile main">
+                
+                <div class="specialist-photo">$photo</div>
+                <div class="specialist-text">
+                    <h3 class="specialist-fio"> $full_name</h3>
+                    <div class="specialist-description">
+                        <p><strong>$experience опыта</strong></p>
+                        <p><strong>$description</strong></p>
+                        <p><strong>Образование:</strong> $education</p>
+                        <p><strong>Специализация:</strong> $specialization</p>
+                    </div>
+                    <div class="specialist-contact">
+                        <div class="specialist-button">[button text="Задать вопрос" link="#specialist-form-$post->ID" class="" expand="true"]</div> <div class="specialist-button-description">Задайте вопрос и $name ответит в течение 1 рабочего дня. Это бесплатно.</div> 
+                    </div>
+                </div>
+                <div class="label">$label</div>
+            </div>
+            $lightbox
+        EOHTML;
+        } else {
+            $html = <<<EOHTML
+            <div class="specialist-tile">
+               
+                <div class="specialist-photo">$photo</div>
+                <div class="specialist-text">
+                 
+                    <h3 class="specialist-fio"> $full_name</h3>
+                    <div class="specialist-description">
+                        <p><strong>$experience опыта</strong></p>
+                        <p><strong>Специализация:</strong> $specialization</p>
+                    </div>
+                </div>
+                <div class="label">$label</div>
+            </div>
+        EOHTML;
+        }
+
+        return $html;
+    }
+
+
+    private function get_specialists_posts(string $category): array
+    {
+        $args = array(
+            'numberposts' => 5,
+            'orderby' => 'date',
+            'order' => 'ASC',
+            'post_type' => 'specialist',
+            'meta_key' => 'specialist_branch',
+            'meta_query' => array(
+                array(
+                    'key' => 'specialist_branch',
+                    'value' => $category,
+                    'compare' => '=',
+                )
+            )
+        );
+        return get_posts($args);
+    }
+
+    private function get_label($category): string
+    {
+        if ($category == "Бухгалтерия") return "Бухгалтер";
+        if ($category == "Аудит") return "Аудитор";
+        if ($category == "Юридические услуги") return "Юрист";
+        if ($category == "Лицензирование") return "Специалист";
+        if ($category == "Регистрация и ликвидация") return "Специалист";
+        return "";
+    }
+
+    private function get_category_by_url(): string
+    {
+        if (strpos($_SERVER['REQUEST_URI'], "services/licensing") !== false) {
+            return "Лицензирование";
+        } else if (strpos($_SERVER['REQUEST_URI'], "services/register-elimination") !== false) {
+            return "Регистрация и ликвидация";
+        } else if (strpos($_SERVER['REQUEST_URI'], "services/services-le") !== false) {
+            return "Юридические услуги";
+        } else if (strpos($_SERVER['REQUEST_URI'], "services/audit") !== false) {
+            return "Аудит";
+        } else if (strpos($_SERVER['REQUEST_URI'], "services/bukhgalterskie-uslugi") !== false) {
+            return "Бухгалтерия";
+        }
+        return "";
+    }
+}
+
+function specialist_block_shortcode($atts)
+{
+    $shortcode = new umbrella_specialist();
+    $shortcode->atts = $atts;
+    if (!$shortcode->fill_attributes()) {
+        return $shortcode->err;
+    }
+    return $shortcode->generate_shortcode();
+}
+
+add_shortcode('specialists', 'specialist_block_shortcode');
