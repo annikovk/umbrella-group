3f50eb9cc14d1ba10572b2ac8a79027231561fa5
konstantin.annikov <konstantin.annikov@jetbrains.com>
2021-10-27 16:03:23 +0700

    accordion improved animation

--- httpdocs/wp-content/themes/umbrella-group/assets/js/main.js
+++ httpdocs/wp-content/themes/umbrella-group/assets/js/main.js
@@ -1,4 +1,11 @@
 jQuery(".accordion-title").click(function (){
-    jQuery(this).parent().parent().find(".accordion-item").removeClass("active");
-    jQuery(this).parent().toggleClass("active");
+    if (jQuery(this).parent().hasClass("active")) {
+        jQuery(this).parent().removeClass("active");
+    } else {
+        jQuery(this).parent().parent().find(".accordion-item").removeClass("active");
+        jQuery(this).parent().addClass("active");
+    }
 });
+jQuery(".accordion button.toggle").click(function (){
+    jQuery(this).parent().find(".accordion-inner").toggle(200);
+})
