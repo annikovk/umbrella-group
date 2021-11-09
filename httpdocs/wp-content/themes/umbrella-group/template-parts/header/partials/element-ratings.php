20c357e737c1196eb49478ad22f9468e618ffdaa
Vsedid <93916468+Vsedid@users.noreply.github.com>
2021-11-09 15:32:15 +0700

    Корректировка блока с рейтингом в хедере

--- httpdocs/wp-content/themes/umbrella-group/template-parts/header/partials/element-ratings.php
+++ httpdocs/wp-content/themes/umbrella-group/template-parts/header/partials/element-ratings.php
@@ -6,11 +6,12 @@ $rating2 =  umbrella_get_ratings('header');
 $ratings_block = <<<EOHTML
         <div class="header-ratings-icons hide-for-medium">
                 <div class="header-ratings-icon-box">
-                    <div class="header-ratings-icon expert-rating" style="background-image: url({$rating1['icon']});"> </div>
+                    <div class="header-ratings-icon " style="background-image: url({$rating2['icon']});"> </div>
+                    <div class="header-ratings-header">{$rating2['header']}</div>
                 </div>
                 <div class="header-ratings-icon-box">
-                    <div class="header-ratings-icon" style="background-image: url({$rating2['icon']});"> </div>
-                    <div class="header-ratings-header">{$rating2['header']}</div>
+                    <div class="header-ratings-icon" style="background-image: url({$rating1['icon']});border-radius:0;"> </div>
+                    <div class="header-ratings-header" style="width: 194px;">{$rating1['header']}</div>
                 </div>
             </div>
 
