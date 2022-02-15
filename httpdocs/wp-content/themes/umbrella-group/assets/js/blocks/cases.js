b729cabf31c4751db1f13d83639daa39cc178ab4
konstantin.annikov <konstantin.annikov@jetbrains.com>
2022-02-15 18:57:30 +0700

    Prevents slider moving on scrolling elements inside this slider

--- httpdocs/wp-content/themes/umbrella-group/assets/js/blocks/cases.js
+++ httpdocs/wp-content/themes/umbrella-group/assets/js/blocks/cases.js
@@ -9,3 +9,8 @@ jQuery(document).ready(function () {
     //If cases block stays right after the feedback block, remove 100px margin-bot from feedback to keep 100px margin between them
     jQuery(".newcases").closest('section').prev().find(".feedback").addClass("margin-bot-minus-thirty");
 });
+
+//Prevents slider moving on scrolling elements inside slider
+document.addEventListener("touchmove", function (e) {
+    jQuery(".slider").off();
+}, {passive: false});
