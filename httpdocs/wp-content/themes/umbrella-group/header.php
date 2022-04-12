327b38f08ec61d69363e28dbfd67b01270b3f62f
Vsedid <93916468+Vsedid@users.noreply.github.com>
2022-04-12 18:27:49 +0700

    Убран какой-то setTimeout

--- httpdocs/wp-content/themes/umbrella-group/header.php
+++ httpdocs/wp-content/themes/umbrella-group/header.php
@@ -57,8 +57,6 @@
 
     <!-- Marquiz script start -->
     <script>
-        document.addEventListener("DOMContentLoaded", function () {
-        setTimeout(function () {
     <?php if ($_SERVER['REQUEST_URI'] == '/blog/%D0%BF%D1%80%D0%B8%D0%B3%D0%BB%D0%B0%D1%88%D0%B0%D0%B5%D0%BC-%D0%BD%D0%B0-%D0%B1%D0%B5%D1%81%D0%BF%D0%BB%D0%B0%D1%82%D0%BD%D1%8B%D0%B9-%D0%B2%D0%B5%D0%B1%D0%B8%D0%BD%D0%B0%D1%80-%D0%B7%D0%B0/'): ?>
         (function (w, d, s, o) {
             var j = d.createElement(s);
@@ -279,8 +277,6 @@
             }
         );
         <?php endif; ?>
-        }, 8000);
-        });
     </script>
     <!-- Marquiz script end -->
 </head>
