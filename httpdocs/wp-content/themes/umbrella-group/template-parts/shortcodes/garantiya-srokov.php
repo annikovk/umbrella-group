7b1af8de0cc125da23f606c518348a933098994c
kirill <kmeleshkin@mail.ru>
2022-06-29 11:28:40 +0700

    Закончил верстку гарантии сроков

--- httpdocs/wp-content/themes/umbrella-group/template-parts/shortcodes/garantiya-srokov.php
+++ httpdocs/wp-content/themes/umbrella-group/template-parts/shortcodes/garantiya-srokov.php
@@ -7,10 +7,47 @@ class garantiya_srokov
     public function generate_shortcode()
     {
         $html = <<<EOHTML
-         [section id='umbrella-feedback' bg_color="rgb(249, 249, 249)" padding="0px"]
+         [section id='umbrella-feedback'  padding="0px"]
            [row]
             [col  span="12" span__sm="12" margin="0px 0px 0px 0px"]
-                <p class="red"> привет </p>
+            <div class="layout main_block box-shadow_block">
+                <div>
+                    <h1 class="title-h1">Гарантия сроков</h1>
+                    <p class="text_content">
+                        В каждом договоре вы увидите срок оказания услуг
+                        и ответственность за нарушение сроков. Время &mdash; самый 
+                        ценный ресурс, и мы знаем цену времени наших клиентов. 
+                        Посмотрите гарантии в договорах:
+                    </p>
+                    <ul class="list_download">
+                        <li><a class="links_download" href="#">Договор аудита</a></li>     
+                        <li><a class="links_download href="#">Договор лицензирования</a></li>     
+                        <li><a class="links_download href="#">Договор бухгалтерского обслуживания</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h1 class="title-h1">Гарантия результата</h1>
+                    <p class="text_content">
+                        Исключаем человеческий фактор <span class="accent_text">двойным контролем:</span>
+                        директора подразделений лично проверяют ведение 
+                        бухгалтерского учёта, аудиторские отчёты и заключения,
+                        участвуют во всех этапах ведения дел в арбитражных 
+                        судах и судах общей юрисдикции. Для <span class="accent_text">комплексного 
+                        решения вопросов</span> подключается команда из аудиторов, 
+                        бухгалтеров и юристов.
+                        <br>
+                        <br>
+                        Аудиторские и бухгалтерские услуги <span class="accent_text">застрахованы &mdash; </span>
+                        при допущении ошибки штрафы и ответственность
+                        ложатся на аудитора.
+                        <br>
+                        <br>
+                        Мы за удобство и комфорт. Поэтому при оформлении 
+                        лицензий вы можете воспользоваться <span class="accent_text">оплатой частями.</span>
+                        100% предоплата не требуется.
+                    </p>
+                </div>
+            </div>
             [/col]
            [/row]
          [/section]
