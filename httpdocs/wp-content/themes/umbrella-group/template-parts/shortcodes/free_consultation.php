08ae6e933fb6abeb3f45e516262380620bf490db
Konstantin Annikov <annikovk@gmail.com>
2022-07-04 21:16:36 +0200

    css fixes

--- httpdocs/wp-content/themes/umbrella-group/template-parts/shortcodes/free_consultation.php
+++ httpdocs/wp-content/themes/umbrella-group/template-parts/shortcodes/free_consultation.php
@@ -12,7 +12,19 @@ class free_consultation
             [row]
                 [col  span="12" span__sm="12" margin="0px 0px 0px 0px"]
                 <img class="mobile_mode_free_consultation" src="https://taxlab.ru/wp-content/uploads/IMG_9591-1.png" alt="IMG_9591-1.png">
-                [contact-form-7 id="13771" title="Форма на главной (бесплатная консультация)"]
+                <div class="bac_form card-shadow-common">
+                <div class="border-form_free_consultation">
+                    <div class="title-h2-text-common margin_title_free_consultation main-text-free_consultation">Бесплатная
+                        консультация
+                    </div>
+                    <div class="main-text-common main-text-free_consultation margin_text_free_consultation">
+                        Специалист тезисно разберёт ваш вопрос на&nbsp;3-минутной бесплатной консультации.
+                        Закажите обратный звонок и&nbsp;мы&nbsp;свяжемся с&nbsp;вами в&nbsp;течение 20&nbsp;минут
+                        в&nbsp;наше рабочее время:пн. &mdash;&nbsp;пт.&nbsp;с&nbsp;09:00 до&nbsp;18:00.
+                    </div>
+                    [contact-form-7 id="13771" title="Форма на главной (бесплатная консультация)"]
+                </div>
+                </div>
                 [/col]
            [/row]
          </div>
--- httpdocs/wp-content/themes/umbrella-group/template-parts/shortcodes/request_call_back.php
+++ httpdocs/wp-content/themes/umbrella-group/template-parts/shortcodes/request_call_back.php
@@ -7,6 +7,7 @@ class request_call_back
     public function generate_shortcode()
     {
         $html = <<<EOHTML
+        
          [section id='request_call_back'  padding="0px"]
             [row]
                 [col  span="12" span__sm="12" margin="0px 0px 0px 0px"]
@@ -14,35 +15,16 @@ class request_call_back
                     <div class="layout_request_call_back">
                         <div class="title-h1-text-common padding-top-request_call_back">Закажите обратный звонок</div>
                         <div class="main-text-common">
-                        Специалист тезисно разберёт ваши вопросы на&nbsp;3-минутной <span class="main-text-accent-common-red">бесплатной консультации</span>. 
-                        Мы&nbsp;свяжемся с&nbsp;вами в&nbsp;течение 20&nbsp;минут 
-                        в&nbsp;рабочее время:пн. &mdash;&nbsp;пт.&nbsp;с&nbsp;09:00 до&nbsp;18:00.
-                    </div>
-                        <div class="grid_form_request_call_back">
-                <div>
-                    <input type="text" placeholder="Имя">
-                    <input type="text" placeholder="+ 7 (ХХХ) ХХХ-ХХ-ХХ *"> 
-                    <input type="text" placeholder="Ваш вопрос">
-                </div>
-                <div class="wrapper_btn_and_checkbox">
-                    <div class="approve_free_consultation">
-                    <input type="checkbox" id="approve" name="approve" checked>
-                            <div class="mute-text-normal-common small-mute-text">
-                                Нажимая на&nbsp;кнопку, вы&nbsp;соглашаетесь с&nbsp;условиями обработки персональных данных
-                            </div>
+                            Специалист тезисно разберёт ваши вопросы на&nbsp;3-минутной <span class="main-text-accent-common-red">бесплатной консультации</span>.
+                            Мы&nbsp;свяжемся с&nbsp;вами в&nbsp;течение 20&nbsp;минут
+                            в&nbsp;рабочее время:пн. &mdash;&nbsp;пт.&nbsp;с&nbsp;09:00 до&nbsp;18:00.
                         </div>
-                        <div class="wrapper_btn_request_call_back">
-                            <div type="submit" class="btn-form_request_call_back">Получить консультацию</div>
-                       </div>
-                        </div>
-                </div>
+                        [contact-form-7 id="13790" title="Главная форма в footer (Обратный звонок)"]
                     </div>
                 </div>
-
-
                 [/col]
            [/row]
-         [/section]
+        [/section]
         EOHTML;
         umbrella_add_custom_css_files(['/assets/css/blocks/request_call_back.css']);
         umbrella_add_custom_js_files(['/assets/js/blocks/request_call_back.js']);
